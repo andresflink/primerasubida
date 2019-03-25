4ab5ef41abe09552cd87271fcfe6ce13bbd308b8
afarboledac <afarboledac@unal.edu.co>
2019-03-25 16:02:05 -0500

    Se calcula El promedio de las notas
    se anade express al package son

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {obtenerPromedio, argv} = require ('./datos.js');
 console.log(argv);
 if(argv._[0] == 'promedio'){
-    console.log('El promedio de ' + argv.n + ' es ');
+    console.log('El promedio de ' + argv.n + ' es ' + obtenerPromedio(argv.m, argv.i, argv.p));
 }else{
     console.log('Promedio no calculado');
 }
